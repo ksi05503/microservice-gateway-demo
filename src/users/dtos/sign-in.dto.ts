014793f64da8119aa20b4783fc585eca0bf7666c
ksi05503 <ksi05503@gmail.com>
2023-01-30 23:52:14 +0900

    feat req validator

--- src/users/dtos/sign-in.dto.ts
+++ src/users/dtos/sign-in.dto.ts
@@ -1,3 +1,15 @@
-export class SignInDto {
+import {IsOptional, IsString} from "class-validator";
 
+export class SignInDto {
+    @IsString()
+    @IsOptional()
+    kakao?: string;
+
+    @IsString()
+    @IsOptional()
+    fb?: string;
+
+    @IsString()
+    @IsOptional()
+    apple?: string;
 }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,11 +1,14 @@
 import { CACHE_MANAGER, Controller, Get, Post,Inject, Body } from '@nestjs/common';
 import { Cache } from 'cache-manager'
 import { UsersService } from './users.service';
+import {SignInDto} from "./dtos/sign-in.dto";
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService,) {}
 
   @Post('sign-in')
-  async signIn(){}
+  async signIn(@Body() signInDto: SignInDto){
+    // ... create session
+  }
 }
