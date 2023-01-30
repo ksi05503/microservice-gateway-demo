0b2ebd318c37567c1913a3729e527c5dd96d59a1
ksi05503 <ksi05503@gmail.com>
2023-01-31 00:03:03 +0900

    feat install proxy runtime bootstrap

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as env from './env'
+import {installCommunityProxy} from "./proxy";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  installCommunityProxy(app);
+
   await app.listen(3000);
 }
 bootstrap();
